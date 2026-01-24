a1a3cb9e26072c72966dfe6faefb539e70891f75
Mohamed Abdalkader <101468654+Mo-Abdalkader@users.noreply.github.com>
2026-01-24 05:09:55 +0200

    Update Feedback.py

--- pages/Feedback.py
+++ pages/Feedback.py
@@ -13,6 +13,9 @@ from config import Config
 from translations import get_text
 from utils.telegram_utils import send_telegram_message, get_telegram_credentials
 
+from utils.session_utils import init_session_state
+
+init_session_state()
 
 def get_t(key):
     """Translation helper"""
@@ -404,4 +407,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
